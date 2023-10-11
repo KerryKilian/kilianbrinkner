76c5b1904a3dcf11eddb5d7a361514da9c6e8269
KerryKilian <kilianbrinkner@gmail.com>
2023-10-11 12:51:49 +0200

    add base tag

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,10 @@ root.render(
   </React.StrictMode>
 );
 
+const baseTag = document.createElement('base');
+baseTag.href = '/kilianbrinkner/';
+document.getElementsByTagName('head')[0].appendChild(baseTag);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
