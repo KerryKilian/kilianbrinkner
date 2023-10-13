3c4e71a75143b3a3e9a6c4dbbe9d5f34066824f1
KerryKilian <kilianbrinkner@gmail.com>
2023-10-13 12:25:32 +0200

    some warnings fixed

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
-import { timelineData } from "../data/Timeline";
 
 interface TimeLineProps {
   title: string;
@@ -25,13 +23,6 @@ export const Slide: React.FC<TimeLineProps> = ({
     <>
       <div className="slide">
         <h3 className="slide__title">{title}</h3>
-        {/* <div className="slide__imgcontainer">
-          <img
-            className="slide__img"
-            src={window.location.origin + "/" + logo}
-            alt=""
-          />
-        </div> */}
         <div className="line"></div>
         <h4 className="slide__cardTitle">{cardTitle}</h4>
         <h4 className="slide__cardSubtitle">{cardSubtitle}</h4>
